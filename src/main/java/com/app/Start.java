93693367b3ce2b7adb1420fcb1f43ebd3d16de7b
GUERIN MATHIS 20240832 <mathis.guerin@efrei.net>
2024-12-19 11:34:10 +0100

    test squash commit

--- src/main/java/com/app/Start.java
+++ src/main/java/com/app/Start.java
@@ -6,6 +6,7 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Hello noob Test!");
         System.out.println("C'est pas la rue");
+        System.out.println("C'est pas la ruiuuuu");
         Menu.afficherMenu();
     }
 }
