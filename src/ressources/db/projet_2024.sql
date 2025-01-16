6067eed27cb1ae0ff146b06f228dec53d8278aee
GUERIN MATHIS 20240832 <mathis.guerin@efrei.net>
2025-01-16 11:01:40 +0100

    update sql dump

--- src/ressources/db/projet_2024.sql
+++ src/ressources/db/projet_2024.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : jeu. 19 déc. 2024 à 10:57
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 16 jan. 2025 à 10:00
 -- Version du serveur : 5.7.36
--- Version de PHP : 8.1.10
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,24 +27,28 @@ SET time_zone = "+00:00";
 -- Structure de la table `programmeur`
 --
 
-CREATE TABLE `programmeur` (
+DROP TABLE IF EXISTS `programmeur`;
+CREATE TABLE IF NOT EXISTS `programmeur` (
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
-  `addresse` varchar(100) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
   `pseudo` varchar(100) NOT NULL,
   `responsable` varchar(100) NOT NULL,
   `hobby` varchar(100) NOT NULL,
   `an_naissance` int(11) NOT NULL,
   `salaire` float NOT NULL,
-  `prime` float NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `prime` float NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `programmeur`
 --
 
-INSERT INTO `programmeur` (`nom`, `prenom`, `addresse`, `pseudo`, `responsable`, `hobby`, `an_naissance`, `salaire`, `prime`) VALUES
-('Mickey', 'Johan', '10 via Della Via', 'Mister Mystic', 'Jean Claude Van Dahm', 'poney ride', 2000, 2900, 20);
+INSERT INTO `programmeur` (`nom`, `prenom`, `adresse`, `pseudo`, `responsable`, `hobby`, `an_naissance`, `salaire`, `prime`, `id`) VALUES
+('a', 'a', 'a', 'a', 'a', 'a', 1, 2, 3, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
